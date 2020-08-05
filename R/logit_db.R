8b9a4932d0e404e0912e93b8bd7b6bb84e684c70
Yilin Ning <yilinning@Yilins-MacBook-Pro-2.local>
2020-08-05 17:58:07 +0800

    Created S3 class and added example data

--- R/logit_db.R
+++ R/logit_db.R
@@ -1,19 +1,19 @@
 #' Double the cases in a dataset
-#' @param dat A dataset.
-#' @param y_name Name or index of the outcome column in \code{dat}.
+#' @param data A dataset.
+#' @param y_name Name or index of the outcome column in \code{data}.
 #' @return Returns a dataset where each case in the input dataset is doubled. An
 #'   ID column (\code{.row_id_db}) is created such that the two rows created
 #'   from the same case has the same ID (the name of this new id column begins
 #'   with \code{.} so as to minimise the risk of overwriting an existing column
 #'   in the input data).
-#' @export
-double_cases <- function(dat, y_name = "y") {
-  dat$.row_id_db <- 1:nrow(dat)
-  dat_cases <- dat[dat[, y_name] == 1, ]
+double_cases <- function(data, y_name = "y") {
+  data$.row_id_db <- 1:nrow(data)
+  dat_cases <- data[data[, y_name] == 1, ]
   dat_cases[, y_name] <- 0
-  dat_db <- rbind(dat_cases, dat)
+  dat_db <- rbind(dat_cases, data)
   dat_db[order(dat_db$.row_id_db), ]
 }
+#' Compute H2 matrix for robust covariance
 compute_h2 <- function(X, r) {
   nr <- nrow(X)
   nc <- ncol(X)
@@ -26,24 +26,29 @@ compute_h2 <- function(X, r) {
 #' Estimate the relative risk from a binary outcome by using the
 #' doubling-of-cases approach
 #' @param formula Formula.
-#' @param weight_name Name or column index in \code{dat} corresponding to the
-#'   sampling weights, if \code{dat} is a case-control sample. Default is
+#' @param weight_name Name or column index in \code{data} corresponding to the
+#'   sampling weights, if \code{data} is a case-control sample. Default is
 #'   \code{NULL}, corresponding to scenarios where the full cohort (or
 #'   cross-sectional data) is available.
-#' @param dat A dataset (without doubling the cases).
-#' @details This function doubles the cases in \code{dat} by using the
-#'   \code{\link{double_cases}} function, and then applies a logistic regression
-#'   to this modified dataset to estimate the relative risk. The robust SE of
-#'   the estimated log relative risk is described in Shouten et al (1993).
+#' @param data A \code{data.frame} or named matrix.
+#' @param ... Other input to \code{\link{glm}}.
+#' @details This function doubles the cases in \code{data}, and then applies a
+#'   logistic regression to this modified dataset to estimate the relative risk.
+#'   The robust SE of the estimated log relative risk is described in Schouten et
+#'   al (1993).
 #'
-#'   If \code{dat} is a (matched) case-control sample, the logistic regression
+#'   If \code{data} is a (matched) case-control sample, the logistic regression
 #'   need to be weighted by the sampling weights corresponding to each subject
-#'   in \code{dat}, where the weight is 1 for each case (if all cases are
+#'   in \code{data}, where the weight is 1 for each case (if all cases are
 #'   selected), and the weight for each control is the number of eligible
 #'   controls divided by the number of controls sampled (counted within each
-#'   sampling stratum if \code{dat} is a matched sample). The robust SE in such
-#'   analyses is modified from Shouten et al (1993) by incorporating the
+#'   sampling stratum if \code{data} is a matched sample). The robust SE in such
+#'   analyses is modified from Schouten et al (1993) by incorporating the
 #'   sampling weights in H2.
+#' @return Returns an object of class \code{db} that inherits from the
+#'   \code{\link{glm}} class. Note that although \code{family} of this object is
+#'   logistic, the underlying model fitted is log-binomial. Items \code{call},
+#'   \code{deviance}, \code{null.deviance} and \code{weights} are not available.
 #' @references
 #' \itemize{
 #'   \item{Schouten, E.G., Dekker, J.M., Kok, F.J., Cessie, S.L., Van
@@ -57,8 +62,29 @@ compute_h2 <- function(X, r) {
 #'   Canadian Medical Association journal, 184(8), 895–899.
 #'   https://doi.org/10.1503/cmaj.101715}
 #' }
+#' @examples
+#' library(DoublingOfCases)
+#' data("cohort_sim")
+#' # Draw a 1:1 case-control sample from cohort:
+#' i_controls <- sample(which(cohort_sim$y == 0), size = sum(cohort_sim$y),
+#'                      replace = FALSE)
+#' dat_cc <- rbind(
+#'   cbind(cohort_sim[cohort_sim$y == 1, ], w = 1),
+#'   cbind(cohort_sim[i_controls, ], w = sum(cohort_sim$y == 0) / length(i_controls))
+#' )
+#' # Estimate relative risk (RR) by using doubling of cases approach:
+#' # Fit the model:
+#' m_cc <- logit_db(y ~ x + male, data = dat_cc, weight_name = "w")
+#' # Extract model summary:
+#' summ_cc <- summary(m_cc)
+#' # Print the estimates log RR, SEs and p-values:
+#' summ_cc$coefficients
+#' # Extract AIC, log-likelihood and fitted outcomes:
+#' AIC(m_cc)
+#' logLik(m_cc)
+#' fitted(m_cc)
 #' @export
-logit_db <- function(formula, weight_name = NULL, dat) {
+logit_db <- function(formula, weight_name = NULL, data, ...) {
   y_name <- as.character(formula[[2]])
   x_fml <- as.character(formula[[3]])
   if (length(x_fml) > 1) {
@@ -67,39 +93,81 @@ logit_db <- function(formula, weight_name = NULL, dat) {
     x_fml <- paste("~", as.character(x_fml))
   }
   if (!is.null(weight_name)) {
-    dat$.weight <- dat[, weight_name]
+    data$.weight <- data[, weight_name]
   } else {
-    dat$.weight <- 1
+    data$.weight <- 1
   }
-  dat_db <- double_cases(dat = dat, y_name = y_name)
+  dat_db <- double_cases(data = data, y_name = y_name)
   m <- glm(formula = formula, family = binomial(link = "logit"), data = dat_db,
-           weights = .weight)
-  coef_mat <- summary(m)$coef
-  H1 <- vcov(m)
-  # Robust SE
-  X <- model.matrix(as.formula(x_fml), data = dat)
+           weights = .weight, ...)
+  # Get fitted values and residuals of log-binomial
+  y <- unlist(data[, y_name])
+  X <- model.matrix(as.formula(x_fml), data = data)
+  m$y <- y
+  m$x <- X
+  m$model <- m$model[!duplicated(dat_db$.row_id_db), ]
   p <- as.vector(exp(X %*% coef(m)))
-  y <- unlist(dat[, y_name])
-  r <- y - (p / (1 + p)) * (y + 1)
-  H2 <- compute_h2(X = X, r = dat$.weight * r)
-  vcov_robust <- H1 %*% H2 %*% H1
-  se_robust <- sqrt(diag(vcov_robust))
-  coef_mat_robust <- data.frame(
-    var = rownames(coef_mat),
-    est = coef_mat[, 1], se_robust = se_robust,
-    pval = 2 * pnorm(abs(coef_mat[, 1] / se_robust), lower.tail = FALSE)
-  )
-  rownames(coef_mat_robust) <- rownames(coef_mat)
+  m$residuals <- y - p
+  m$linear.predictors <- log(p)
+  m$fitted.values <- p
+  m$df.null <- nrow(data)
+  m$df.residual <- nrow(data) - length(coef(m))
+  m$prior.weights <- data$.weight
   if (any(p > 1)) {
     warning(simpleWarning(sprintf("%d (%.1f%%) of the %d subjects have estimated probability >1.",
-                                  sum(p > 1), mean(p > 1) * 100, nrow(dat))))
+                                  sum(p > 1), mean(p > 1) * 100, nrow(data))))
     llh <- NA
   } else {
-    llh <- sum(dat$.weight * y * log(p) + dat$.weight * (1 - y) * log(1 - p))
+    llh <- sum(data$.weight * y * log(p) + data$.weight * (1 - y) * log(1 - p))
   }
-  list(obj = m, coef_mat_robust = coef_mat_robust, vcov_robust = vcov_robust,
-       fitted_values = as.vector(p), llh = llh,
-       AIC = 2 * length(coef(m)) - 2 * llh,
-       BIC = log(nrow(dat)) * length(coef(m)) - 2 * llh)
-       # gof_pval = generalhoslem::logitgof(obs = y, exp = as.vector(p))$p.value)
+  m$log_likelihood <- llh
+  m$aic <- 2 * length(coef(m)) - 2 * llh
+  m$H1 <- vcov(m)
+  # The following are not yet implemented:
+  m$call <- NULL #**** Not sure if can make this the correct call to logit_db
+  m$deviance <- NULL
+  m$null.deviance
+  m$weights <- NULL
+  class(m) <- c("db", "glm", "lm")
+  m
+}
+#' @describeIn summary.db
+#' Extract corrected covariance matrix
+#' @export
+vcov.db <- function(object, ...) {
+  r <- object$y - (object$fitted.values / (1 + object$fitted.values)) * (object$y + 1)
+  H2 <- compute_h2(X = object$x, r = object$prior.weights * r)
+  object$H1 %*% H2 %*% object$H1
+}
+#' @describeIn summary.db
+#' Extract log-likelihood with respect to the log-binomial model of interest
+#' @export
+logLik.db <- function(object, ...) {
+  object$log_likelihood
+}
+#' @describeIn summary.db
+#' Extract AIC
+#' @export
+AIC.db <- function(object, ..., k = 2) {
+  k * length(coef(object)) - 2 * logLik(object)
+}
+#' Summarise the fitted model for the doubling of cases approach
+#' @param object The model fitted using \code{\link{logit_db}}.
+#' @param ... Additional input.
+#' @details See usage in \code{\link{logit_db}}.
+#' @export
+summary.db <- function(object, ...) {
+  vcov_robust <- vcov(object)
+  se_robust <- sqrt(diag(vcov_robust))
+  coefficients <- data.frame(
+    est = coef(object), se_naive = sqrt(diag(object$H1)),
+    se_robust = se_robust,
+    pval = 2 * pt(q = abs(coef(object) / se_robust), df = object$df.residual,
+                  lower.tail = FALSE)
+  )
+  print(coefficients, digits = 4)
+  list(call = object$call, aic = object$aic,
+       df = c(object$rank, object$df.residual, length(coef(object))),
+       coefficients = coefficients,
+       cov.unscaled = vcov_robust, cov.scaled = vcov_robust)
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+Use the following code to install the package:
+```r
+# Package `devtools` required
+devtools::install_github("nyilin/DoublingOfCases")
+```
+
+This package offers a function `logit_db` that estimates relative risk in
+studies of binary outcomes by using the doubling-of-cases approach.
